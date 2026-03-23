f7891ed76d17bfd88cc55f1c9c766371c7dbd73b
Sreesharan007 <sreesharan.p.n26@gmail.com>
2026-03-23 23:25:15 +0530

    Create script2_package_inspector.sh for package checks
    Added a package inspector script to check for installed packages and provide descriptions.

--- script2_package_inspector.sh
+++ script2_package_inspector.sh
@@ -1,1 +1,19 @@
+#!/bin/bash
+# Script 2: FOSS Package Inspector
 
+PACKAGE="git"
+
+if dpkg -l | grep -q $PACKAGE; then
+    echo "$PACKAGE is installed."
+    git --version
+else
+    echo "$PACKAGE is NOT installed."
+fi
+
+case $PACKAGE in
+    git) echo "Git: Distributed version control system built for open collaboration" ;;
+    vlc) echo "VLC: Open-source multimedia player" ;;
+    firefox) echo "Firefox: Open web browser promoting privacy" ;;
+    apache2) echo "Apache: Web server powering the internet" ;;
+    *) echo "Unknown package" ;;
+esac
